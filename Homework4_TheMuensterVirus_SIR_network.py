33030ea27829c619e200c9fedea0a4dfecaf3fca
jv00nlp <93138902+jv00nlp@users.noreply.github.com>
2021-11-23 19:25:43 +0100

    Add files via upload

--- Homework4_TheMuensterVirus_SIR_network.py
+++ Homework4_TheMuensterVirus_SIR_network.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 16 13:38:52 2021
+Created on Mon Nov 22 15:27:25 2021
 
 @author: Julian
 """
 
-'''
-Homework 4 - Task 6: The Münster-Virus
-
-'''
-
 import numpy as np
-#import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt
 
 def rk4_step(rhs, x, function_parameters, h):
     k1 = rhs(x, *function_parameters)
@@ -21,44 +15,55 @@ def rk4_step(rhs, x, function_parameters, h):
     k4 = rhs(x + k3*h, *function_parameters)
     return x + h/6.*(k1 + 2*(k2 + k3) + k4)
 
-def SIR(U, alpha, beta, n, matrix, S_dummy, I_dummy, R_dummy):
-    # pass vectors of len(M), wrapped together in U
+def SIR(U, alpha, beta, matrix):
     S, I, R = U
-    
-    
     N = np.zeros(len(matrix))
     
-    for i in range(0,len(M)):
+    for i in range(0,len(matrix)):
         N[i] = Sn[0,i] + In[0,i] + Rn[0,i]
-        
-    # these should have the same dimension as S, I, R, i.e. a len(M) vector    
-    sum_S = 0
-    sum_I = 0
-    sum_R = 0
+   
+    dS = - alpha*S*I/N
+    dI = alpha*S*I/N - beta*I
+    dR =  beta*I 
     
-    for n in range(0, len(M)):
-        for m in range(0,len(M)):
+
+
+    for i in range(0,len(matrix)):
+        N[i] = Sn[0,i] + In[0,i] + Rn[0,i]
+    
+    for n in range(0,len(matrix)):
+            for m in range(0,len(matrix)):
                 
-            sum_S = sum_S + matrix[n,m] * S_dummy[m] - matrix[m,n] * S
-            sum_I = sum_I + matrix[n,m] * I_dummy[m] - matrix[m,n] * I
-            sum_R = sum_R + matrix[n,m] * R_dummy[m] - matrix[m,n] * R
-        
-    dS = -alpha*S*I/N[n] + sum_S
-    dI = alpha*S*I/N[n] - beta*I + sum_I
-    dR = beta*I + sum_R
+                if m != n:
+                
+                    
+#                    dS[n] = dS[n] + matrix[n,m] * S[m] - matrix[m,n] * S[n]     
+#                    dI[n] = dI[n] + matrix[n,m] * I[m] - matrix[m,n] * I[n]
+#                    dR[n] = dR[n] + matrix[n,m] * R[m] - matrix[m,n] * R[n]
+                    
+                    '''
+                    hier entsteht der Fehler, in S[m]/S[n],R[m] etc..
+                    
+                    '''
+                    
+                    dS[n] = dS[n] + matrix[n,m]  - matrix[m,n] 
+                    dI[n] = dI[n] + matrix[n,m]  - matrix[m,n] 
+                    dR[n] = dR[n] + matrix[n,m]  - matrix[m,n] 
+                      
+                
     return np.array([dS, dI, dR])
+
+# parameters
+dt = 0.1
+Tend = 150
+Nt = int(Tend/dt)
+ts = np.linspace(0, Tend, Nt)
 
 alpha = 0.35
 beta = 0.035
 
-# parameters
-dt = 0.1
-Tend = 15
-Nt = int(Tend/dt)
-ts = np.linspace(0, Tend, Nt)
 
-
-
+# load extern data
 adj_matrix  =   np.loadtxt('AdjacencyMuenster.csv', delimiter = ',', skiprows = 1, usecols = (1,2,3,4,5,6,7,8,9,10,11))
 pop         =   np.loadtxt('Populations.csv', delimiter = ',', usecols = (1)) 
 
@@ -71,18 +76,19 @@ In[0,0] = 20                     # let virus start in Muenster
 Sn[0,0] = Sn[0,0] - In[0,0]     # in order to make that total start population in Münster is still according to 'Populations.csv'
 Rn = np.zeros((len(ts),len(M)))
 
+U = [Sn[0,:], In[0,:], Rn[0,:]]
 
-
-# for n in range(0,len(M)):
-    
 for i in range(1,Nt):
-    # here u should wrap together the len(M) vectors S, I, R
-    U = [Sn[i,n], In[i,n], Rn[i,n]]
-    S = Sn[i,:]
-    I = In[i,:]
-    R = Rn[i,:]
-    U = rk4_step(SIR, U, [alpha, beta, n, M, S, R, I], dt)
-    Sn[i,n], In[i,n], Rn[i,n] = U
-        
+    U = rk4_step(SIR, U, [alpha, beta, M], dt)
+    Sn[i], In[i], Rn[i,:] = U   # Sn[i] funktioniert hier wie Sn[i,:]
+    
 
+y = In
+
+plt.plot(ts, Sn[:,0], 'C0-')
+plt.plot(ts, In[:,0], 'C3-')
+plt.plot(ts, Rn[:,0], 'C2-')
+plt.title('S I R model  with  ' r'$\beta='+ str(alpha)+'$' '  &  ' r'$\gamma =' + str(beta)+'$', fontweight="bold")
+
+plt.show()
 
