f99d1a5c928b75f43dc152bdfd71f88ec4745710
Alexander <alexandertiekoetter1@gmail.com>
2022-01-03 14:35:01 +0100

    Added TODOs

--- SIR_Model_Network.py
+++ SIR_Model_Network.py
@@ -81,8 +81,11 @@ def sir_as_network(compartment, infection_rate, removal_rate, adjacency_matrix_p
 
     sir_adjacency_matrix = replace_placeholders(sir_adjacency_matrix, sir_placeholders, sir_placeholder_values,
                                                 sir_placeholder_categories)
-    # TODO: Create a value list for the variables in all cities and then figure out how to return a result array that
-    #  looks the same like in "sir"
+    # TODO: Change compartment variable into 11D Array with 3 entries (S, I, R eg.) instead of 3D Array with 11 entries
+    # TODO: Calculate total population
+    # TODO: Replace every Variable in Matrix with its current values
+    # TODO: Iterate over NUMBER_OF_CITIES and calc the dS, dI, dR for each city
+    # TODO: Reparse the result array to match with former sir - function
 
     result = np.matmul(np.transpose(sir_adjacency_matrix), compartment)
     return result
